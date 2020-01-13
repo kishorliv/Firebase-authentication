f0f8a760598be14a485a867a659b61d25a5753cb
kishor neupane <kishorliv@gmail.com>
2020-01-13 22:19:25 +1100

    Update route paths.

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,4 @@
-export const LANDING = '/';
-export const HOME = '/home';
+export const HOME = '/';
 export const ADD_ISSUE = '/add-issue';
 export const SIGN_IN = '/signin';
 export const SIGN_UP = '/signup';
